bfeb2382109e86e82f21f2107da2734bca886d16
Wukong <BitcoinWukong@proton.me>
2023-11-26 00:35:11 +0000

    wip: further improve the UI of TakeOrderDialog

--- app/src/main/java/com/bitcoinwukong/robosats_android/ui/order/TakeOrderDialog.kt
+++ app/src/main/java/com/bitcoinwukong/robosats_android/ui/order/TakeOrderDialog.kt
@@ -1,11 +1,16 @@
 package com.bitcoinwukong.robosats_android.ui.order
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.bitcoinwukong.robosats_android.model.OrderData
+import com.bitcoinwukong.robosats_android.model.OrderType
 import com.bitcoinwukong.robosats_android.model.PaymentMethod
 
 @Composable
@@ -16,16 +21,19 @@ fun TakeOrderDialog(orderData: OrderData?, onDismiss: () -> Unit, onTakeOrder: (
             title = { Text("Take Order") },
             text = {
                 Column {
-                    Text("Order ID: ${orderData.id ?: "N/A"}")
-                    Text("Type: ${orderData.type.name}")
-                    Text("Currency: ${orderData.currency.code}")
-                    Text("Amount: ${orderData.amount ?: "N/A"}")
-                    Text("Min Amount: ${orderData.minAmount ?: "N/A"}")
-                    Text("Max Amount: ${orderData.maxAmount ?: "N/A"}")
-                    if (orderData.paymentMethod == PaymentMethod.CUSTOM) {
-                        Text("Payment Method: ${orderData.customPaymentMethod ?: "N/A"}")
+                    if (orderData.type == OrderType.BUY) {
+                        Text("You're Selling Bitcoin")
                     } else {
-                        Text("Payment Method: ${orderData.paymentMethod.name}")
+                        Text("You're Buying Bitcoin")
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text("Order ID: ${orderData.id ?: "N/A"}")
+                    Text("Currency: ${orderData.currency.code}")
+                    Text("Amount: ${orderData.formattedAmount()}")
+                    if (orderData.paymentMethod == PaymentMethod.CUSTOM) {
+                        Text("Payment: ${orderData.customPaymentMethod ?: "N/A"}")
+                    } else {
+                        Text("Payment: ${orderData.paymentMethod.methodName}")
                     }
                     Text("Price: ${orderData.price ?: "N/A"}")
                     Text("Premium: ${orderData.premium ?: "N/A"}%")
@@ -33,7 +41,7 @@ fun TakeOrderDialog(orderData: OrderData?, onDismiss: () -> Unit, onTakeOrder: (
             },
             confirmButton = {
                 Button(onClick = { onTakeOrder() }) {
-                    Text("Take Order")
+                    Text("Accept")
                 }
             },
             dismissButton = {
