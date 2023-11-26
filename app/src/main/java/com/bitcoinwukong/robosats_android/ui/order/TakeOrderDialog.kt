1abdee1247b3b134c3718fdd1f399b59f7556b37
Wukong <BitcoinWukong@proton.me>
2023-11-26 00:30:31 +0000

    wip: fill in the order details in the take order dialog

--- app/src/main/java/com/bitcoinwukong/robosats_android/ui/order/TakeOrderDialog.kt
+++ app/src/main/java/com/bitcoinwukong/robosats_android/ui/order/TakeOrderDialog.kt
@@ -1,10 +1,12 @@
 package com.bitcoinwukong.robosats_android.ui.order
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import com.bitcoinwukong.robosats_android.model.OrderData
+import com.bitcoinwukong.robosats_android.model.PaymentMethod
 
 @Composable
 fun TakeOrderDialog(orderData: OrderData?, onDismiss: () -> Unit, onTakeOrder: () -> Unit) {
@@ -12,10 +14,31 @@ fun TakeOrderDialog(orderData: OrderData?, onDismiss: () -> Unit, onTakeOrder: (
         AlertDialog(
             onDismissRequest = { onDismiss() },
             title = { Text("Take Order") },
-            text = { /* Display order details here */ },
+            text = {
+                Column {
+                    Text("Order ID: ${orderData.id ?: "N/A"}")
+                    Text("Type: ${orderData.type.name}")
+                    Text("Currency: ${orderData.currency.code}")
+                    Text("Amount: ${orderData.amount ?: "N/A"}")
+                    Text("Min Amount: ${orderData.minAmount ?: "N/A"}")
+                    Text("Max Amount: ${orderData.maxAmount ?: "N/A"}")
+                    if (orderData.paymentMethod == PaymentMethod.CUSTOM) {
+                        Text("Payment Method: ${orderData.customPaymentMethod ?: "N/A"}")
+                    } else {
+                        Text("Payment Method: ${orderData.paymentMethod.name}")
+                    }
+                    Text("Price: ${orderData.price ?: "N/A"}")
+                    Text("Premium: ${orderData.premium ?: "N/A"}%")
+                }
+            },
             confirmButton = {
                 Button(onClick = { onTakeOrder() }) {
                     Text("Take Order")
+                }
+            },
+            dismissButton = {
+                Button(onClick = { onDismiss() }) {
+                    Text("Cancel")
                 }
             }
         )
